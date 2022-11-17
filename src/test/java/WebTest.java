d23f8f57f10433c83a0c6faea79556599905467e
MargoSamo <margo_ii@mail.ru>
2022-11-17 19:08:56 +0500

    First test Selenium

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class WebTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTest {
+
+    public static void main(String[] args) {
+
+        WebDriver auto = new ChromeDriver();
+//        auto.get("https://ibs.ru/");
+//        auto.findElement(By.className("header-search")).click();
+        auto.get("https://ibs.ru/contacts/");
+//        auto.findElement(By.xpath("//div[@class='header-link']//a[@class='/contacts/']")).click();
+        auto.findElement(By.id("bx_651765591_2403")).click();
+    }
 }
